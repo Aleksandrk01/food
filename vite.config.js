f1aae392d038856bf7cdb76ba26a86ab106b6670
Aleksandr <dnepr03.0304@gmail.com>
2024-12-06 14:55:26 +0200

    bild

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Убедитесь, что это значение установлено
-});
+// В tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
