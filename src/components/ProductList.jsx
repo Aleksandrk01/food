c32d1169e9fb4d62b7a162f5adf148b65adb81b8
Aleksandr <dnepr03.0304@gmail.com>
2024-12-08 01:39:35 +0200

    burder

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,38 +3,121 @@ import React from 'react';
 import '../css/ProductList.css';
 import ProductCard from '../components/ProductCard';
 import Product1 from '../assets/images/product1.jpg';
-import Product2 from '../assets/images/product2.jpg';
+// import Product2 from '../assets/images/product2.jpg';
 
 const products = [
   {
     id: 1,
-    name: 'Beef Burger',
+    name: 'Grizzly Standart',
     description: 'Saftiger Rindfleisch-Burger mit frischen Zutaten.',
     image: Product1,
-    price: '8,99',
+    price: '11,90',
   },
   {
     id: 2,
-    name: 'Chicken Burger',
+    name: 'Grizzly Oko (veggiel)',
     description: 'Zarter Hähnchen-Burger mit knackigem Salat.',
-    image: Product2,
-    price: '7,99',
+    image: Product1,
+    price: '11,90',
   },
   {
     id: 3,
-    name: 'Veggie Burger',
+    name: 'Cheesseburger',
     description: 'Leichter Gemüse-Burger für Vegetarier.',
-    image: Product2,
-    price: '6,99',
+    image: Product1,
+    price: '12,90',
   },
   {
     id: 4,
+    name: 'Bavernburger mit El',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '13,90',
+  },
+  {
+    id: 5,
+    name: 'Grizzly HOT',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '13,90',
+  },
+  {
+    id: 6,
+    name: 'Gitzzly Bacon',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '15,90',
+  },
+  {
+    id: 7,
+    name: 'Grizzly BBQ',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '15,90',
+  },
+  {
+    id: 8,
+    name: 'Grizzly Premium XXL',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '17,90',
+  },
+  {
+    id: 9,
+    name: 'Grizzly XXXL',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '22,90',
+  },
+  {
+    id: 10,
+    name: 'Chicken Burger',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '8,90',
+  },
+  {
+    id: 11,
+    name: 'Spicy Chicken',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '8,90',
+  },
+  {
+    id: 12,
+    name: 'Chicken Bauer mit Ei',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '10,90',
+  },
+  {
+    id: 13,
+    name: 'Grizzly Chicken Burger',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '14,90',
+  },
+  {
+    id: 14,
     name: 'Veggie Burger',
     description: 'Leichter Gemüse-Burger für Vegetarier.',
-    image: Product2,
-    price: '6,99',
+    image: Product1,
+    price: '15,90',
   },
-  // Добавьте больше товаров по необходимости
+  {
+    id: 15,
+    name: 'Veggie Cheeses',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '8,90',
+  },
+  {
+    id: 16,
+    name: 'Veggie HOT',
+    description: 'Leichter Gemüse-Burger für Vegetarier.',
+    image: Product1,
+    price: '8,90',
+  }
 ];
 
 const ProductList = () => {
